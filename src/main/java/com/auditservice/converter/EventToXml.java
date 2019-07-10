58d822b7148e0bc6821e718ae70c815436d14a75
sumitGitHub1 <sumit.smile@gmail.com>
2019-07-10 18:54:08 +0530

    Code Checkd-in

--- src/main/java/com/auditservice/converter/EventToXml.java
+++ src/main/java/com/auditservice/converter/EventToXml.java
@@ -9,19 +9,19 @@ import org.springframework.jms.core.JmsTemplate;
 
 import com.auditservice.main.AuditServiceApplicationStarter;
 
-import event.generator.EventGenerator;
+//import event.generator.EventGenerator;
 
 public class EventToXml {
 	public static void main(String[] args) throws Exception {
 
-		EventGenerator eG = new EventGenerator();
-		String eventXML = eG.generateEvent("Sumit12", "Login-Module", "login", "additional", "Sumit", "passed",
-				LocalDateTime.now(ZoneId.systemDefault()), LocalDateTime.now(ZoneId.systemDefault()));
-		System.out.println(eventXML);
-		ApplicationContext ctx = SpringApplication.run(
-				AuditServiceApplicationStarter.class, args);
-		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
-		jms.convertAndSend("event.queue", eventXML);
-		
+//		EventGenerator eG = new EventGenerator();
+//		String eventXML = eG.generateEvent("Sumit12", "Login-Module", "login", "additional", "Sumit", "passed",
+//				LocalDateTime.now(ZoneId.systemDefault()), LocalDateTime.now(ZoneId.systemDefault()));
+//		System.out.println(eventXML);
+//		ApplicationContext ctx = SpringApplication.run(
+//				AuditServiceApplicationStarter.class, args);
+//		JmsTemplate jms = ctx.getBean(JmsTemplate.class);
+//		jms.convertAndSend("event.queue", eventXML);
+//		
 	}
 }
